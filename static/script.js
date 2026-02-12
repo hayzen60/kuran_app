bcc4761ecea6a84701c408826a224a6b95894fe9
hayzen60 <hikalif470@gmail.com>
2026-02-12 17:29:17 +0300

    dark mode added

--- static/script.js
+++ static/script.js
@@ -1,3 +1,11 @@
+// Tema yükleme
+document.addEventListener("DOMContentLoaded", function () {
+    const theme = localStorage.getItem("theme");
+    if (theme === "dark") {
+        document.body.classList.add("dark");
+    }
+});
+
 // Sure isimleri
 const surahNames = {
     "1": "Fatiha",
@@ -86,3 +94,13 @@ document.addEventListener("DOMContentLoaded", function () {
 
     container.appendChild(btn);
 });
+
+function toggleTheme() {
+    document.body.classList.toggle("dark");
+
+    if (document.body.classList.contains("dark")) {
+        localStorage.setItem("theme", "dark");
+    } else {
+        localStorage.setItem("theme", "light");
+    }
+}
